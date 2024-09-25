cdef2a315ff4bb26867326b0688ac30852f0e34c
EnverJ <Enlem1987@gmail.com>
2024-09-24 22:21:06 -0600

    Java Code practice

---
+++ src/test/java/Java30Coding/StringContains.java
@@ -0,0 +1,9 @@
+package Java30Coding;
+
+public class StringContains {
+    public static void main(String [] args){
+        String str1="Automation Testing";
+        String str2="Testing";
+        System.out.println(str1.contains(str2));
+    }
+}
